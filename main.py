52139013055b8a45ca564f0962bdda0a31644554
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-10-17 00:29:39 +0530

    hacker new api integrated and exposed

--- main.py
+++ main.py
@@ -4,22 +4,36 @@ from pprint import pprint as p
 
 app = Flask(__name__)
 
+story_url = "https://hacker-news.firebaseio.com/v0/item/"
+top_stories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+stories_n = 2
+
 
 @app.route("/news/<category>")
 def news_page(category):
-    url = f"https://saurav.tech/NewsAPI/top-headlines/category/{category}/in.json"
-    articles = requests.get(url).json()["articles"]
-    data = make_posts(articles)
-    return render_template("new.html", data=data, title=articles[0]["title"])
+    url = get_news_url(category)
+    data = get_posts(url)
+    hacker_posts = get_hacker_posts()
+    return render_template(
+        "new.html",
+        data=data,
+        title=data[0]["title"],
+        hacker_posts=hacker_posts,
+    )
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    url = f"https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
-    articles = requests.get(url).json()["articles"]
-    data = make_posts(articles)
-    return render_template("home.html", data=data, title="General news")
+    url = get_news_url()
+    data = get_posts(url)
+    hacker_posts = get_hacker_posts()
+    return render_template(
+        "home.html",
+        data=data,
+        title="General news",
+        hacker_posts=hacker_posts,
+    )
 
 
 def make_posts(articles: list) -> list:
@@ -34,11 +48,47 @@ def make_posts(articles: list) -> list:
         post["sourceUrl"] = article["url"]
         post["time"] = article["publishedAt"]
         if "youtube" in post["sourceName"]["name"].lower():
-            post["youtube"] = f"https:/youtube.com/embed/{post['sourceUrl'].split('=')[-1]}"
-            p("-"*30)
-            p(post["youtube"])
+            post[
+                "youtube"
+            ] = f"https:/youtube.com/embed/{post['sourceUrl'].split('=')[-1]}"
         data.append(post)
     return data
+
+
+def get_news_url(category="general"):
+    return f"https://saurav.tech/NewsAPI/top-headlines/category/{category}/in.json"
+
+
+def get_posts(url):
+    articles = requests.get(url).json()["articles"]
+    data = make_posts(articles)
+    return data
+
+
+# make urls
+def make_urls(story_ids: list[str]) -> str:
+    for story_id in story_ids:
+        url = f"{story_url}{story_id}.json"
+        yield url
+
+
+def get_hacker_ids(stories_n) -> list[str]:
+    story_ids = requests.get(top_stories_url).json()
+    return story_ids[:stories_n]
+
+
+# get story data
+def get_story(story_url) -> dict:
+    data = requests.get(story_url).json()
+    return data
+
+
+def get_hacker_posts() -> list[dict]:
+    hacker_posts = []
+    hacker_ids = get_hacker_ids(stories_n)
+    for url in make_urls(hacker_ids):
+        hacker_posts.append(get_story(url))
+    return hacker_posts
 
 
 if __name__ == "__main__":
